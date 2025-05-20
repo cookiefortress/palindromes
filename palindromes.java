34dd5f6cc5479eff4d3e2ddfab6be9ef677026fa
cookiefortress <iwantcookieplz@gmail.com>
2025-05-20 19:18:22 -0400

    Implement up to the point of reversing the string input by the user. Only using ArrayList objects currently.

--- palindromes.java
+++ palindromes.java
@@ -1,14 +1,51 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class palindromes {
-    public static void main(String[] args) {
+    public static String getText() {
         Scanner palindromeScanner = new Scanner(System.in);
 
         System.out.print("Enter the word you would like to test for palindromicity! \n\n");
-        String text = palindromeScanner.nextLine(); // Program waits here for input
-
+        String text = palindromeScanner.nextLine();
         System.out.println("\nAllow me to check if '" + text + "' is a palindrome...");
-
         palindromeScanner.close();
+        return text;
+    }
+
+    public static ArrayList<Character> sanitizeString(String text) {
+        ArrayList<Character> textArr = new ArrayList<>();
+        for(char c : text.toCharArray()) {
+            if(Character.isLetterOrDigit(c)) {
+                textArr.add(c);
+            }
+        }
+        return textArr;
+    }
+
+    public static ArrayList<Character> reverseArr(ArrayList<Character> textArr) {
+        ArrayList<Character> reversedArray = new ArrayList<>();
+
+        for (int i = 0; i < textArr.size(); i++) {
+            reversedArray.add(textArr.get(textArr.size() - 1 - i));
+        }
+
+        return reversedArray;
+    }
+
+    public static String convertToString(ArrayList<Character> textArr) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : textArr) {
+            sb.append(ch);
+        }
+        String result = sb.toString();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String text = getText();
+        ArrayList<Character> sanitizedArr = sanitizeString(text);
+        ArrayList<Character> reversedArr = reverseArr(sanitizedArr);
+        String reversedText = convertToString(reversedArr);
+        System.out.println(reversedText);
     }
 }
